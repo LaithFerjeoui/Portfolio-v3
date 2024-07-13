f190d5d8a500c2aaea466d76f6f20dc50eec5bf9
Laith Ferjeoui <124667096+LaithFerjeoui@users.noreply.github.com>
2024-07-13 15:48:24 +0200

    Added gap in phone view in the footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -31,12 +31,12 @@ const Footer = () => {
                     />
                 </a>
             </div>
-            <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+            <div className="flex mt-16 md:flex-row flex-col justify-between items-center gap-y-4">
                 <p className="md:text-base text-sm md:font-normal font-light">
                     Copyright © 2024 Laith Ferjeoui
                 </p>
 
-                <div className="flex items-center md:gap-3 gap-6">
+                <div className="flex items-center md:gap-3 gap-6 ">
                     {socialMedia.map((info) => (
                         <a
                             key={info.id}
